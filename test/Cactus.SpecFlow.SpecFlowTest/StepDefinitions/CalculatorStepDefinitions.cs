5dd7110d36dca579e0ef89902b75d2e638e7a55a
Chunxing Shen <chunxing.shen@gmail.com>
2024-03-07 00:20:13 -0600

    #1 Let me finish what i left on specflow before switch

--- test/Cactus.SpecFlow.SpecFlowTest/StepDefinitions/CalculatorStepDefinitions.cs
+++ test/Cactus.SpecFlow.SpecFlowTest/StepDefinitions/CalculatorStepDefinitions.cs
@@ -1,9 +1,14 @@
+using NUnit.Framework;
+
 namespace Cactus.SpecFlow.SpecFlowTest.StepDefinitions
 {
     [Binding]
     public sealed class CalculatorStepDefinitions
     {
         // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+        int _firstNumber;
+        int _secondNumber;
+        int _result;
 
         [Given("the first number is (.*)")]
         public void GivenTheFirstNumberIs(int number)
@@ -14,7 +19,8 @@ namespace Cactus.SpecFlow.SpecFlowTest.StepDefinitions
             // additional string/Table parameters can be defined on the step definition
             // method. 
 
-            throw new PendingStepException();
+            //throw new PendingStepException();
+            _firstNumber = number;
         }
 
         [Given("the second number is (.*)")]
@@ -22,7 +28,8 @@ namespace Cactus.SpecFlow.SpecFlowTest.StepDefinitions
         {
             //TODO: implement arrange (precondition) logic
 
-            throw new PendingStepException();
+            //throw new PendingStepException();
+            _secondNumber = number;
         }
 
         [When("the two numbers are added")]
@@ -30,7 +37,8 @@ namespace Cactus.SpecFlow.SpecFlowTest.StepDefinitions
         {
             //TODO: implement act (action) logic
 
-            throw new PendingStepException();
+            //throw new PendingStepException();
+            _result = _firstNumber + _secondNumber;
         }
 
         [Then("the result should be (.*)")]
@@ -38,7 +46,8 @@ namespace Cactus.SpecFlow.SpecFlowTest.StepDefinitions
         {
             //TODO: implement assert (verification) logic
 
-            throw new PendingStepException();
+            //throw new PendingStepException();
+            Assert.AreEqual(result, _result);
         }
     }
 }
