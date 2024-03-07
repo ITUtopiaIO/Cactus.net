68b0f1a8eab1c4066e5d17351597f82d0d674b1d
Chunxing Shen <chunxing.shen@gmail.com>
2024-03-07 00:27:17 -0600

    #1 Let me finish what i left on specflow before switch

--- test/Cactus.SpecFlow.SampleTest/Features/Calculator.feature.cs
+++ test/Cactus.SpecFlow.SampleTest/Features/Calculator.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Cactus.SpecFlow.SpecFlowTest.Features
+namespace Cactus.SpecFlow.SampleTest.Features
 {
     using TechTalk.SpecFlow;
     using System;
