f6d99285fb7d4c74033612db551b9a0e779c10d3
Chunxing Shen <chunxing.shen@gmail.com>
2024-04-21 23:33:39 -0500

    #2 Update MiniExcel Test to read value

--- test/Cactus.MiniExcelNUnitTest/BasicTest.cs
+++ test/Cactus.MiniExcelNUnitTest/BasicTest.cs
@@ -17,7 +17,16 @@ namespace Cactus.MiniExcelNUnitTest
         {
             var Rows = MiniExcel.Query(_path).ToList();
 
-            Assert.That(Rows.Count, Is.EqualTo(2));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(Rows.Count, Is.EqualTo(2));
+
+                Assert.That(((String)Rows[0].A).Trim(), Is.EqualTo("Test"));
+                Assert.That(((String)Rows[0].B).Trim(), Is.EqualTo("Sample"));
+                Assert.That(Rows[1].A, Is.EqualTo(1));
+                Assert.That(Rows[1].B, Is.EqualTo(2));
+            });
         }
     }
 }
