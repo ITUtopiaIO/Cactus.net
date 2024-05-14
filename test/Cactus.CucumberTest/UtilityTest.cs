e846c37ee83beb0f70926031724405d5e4bca3c9
Chunxing Shen <chunxing.shen@gmail.com>
2024-05-14 00:04:49 -0500

    #4 Add File Compare Utility

---
+++ test/Cactus.CucumberTest/UtilityTest.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework.Internal;
+using DiffPlex;
+using DiffPlex.Model;
+
+namespace Cactus.CucumberTest
+{
+    public class UtilityTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Differ differ = new Differ();
+            var diff = differ.CreateLineDiffs("Hello   world!", "Hello world!", true);
+            Assert.NotNull(diff);
+            Assert.That(diff.DiffBlocks.Count, Is.EqualTo(1));
+                
+            
+        }
+    }
+}
