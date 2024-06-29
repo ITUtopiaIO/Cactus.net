f18ede552f32ecb3d55d49f054a92042e2893799
Chunxing Shen <chunxing.shen@gmail.com>
2024-06-29 18:13:30 -0500

    #2 Add mock up file compare

--- test/Cactus.MiniExcelConverterTest/Features/Converter.feature
+++ test/Cactus.MiniExcelConverterTest/Features/Converter.feature
@@ -6,4 +6,5 @@ Scenario: Convert a simple Excel file to a feature file
 	Given I have an Excel file "SampleTest.xlsx"
 	When I convert the Excel file to a feature file
 	Then I should have a feature file "SampleTest.feature"
+	And the feature file should match with "SampleTest.exp"
 	
--- test/Cactus.MiniExcelConverterTest/StepDefinitions/MiniExcelConverterStepDefinitions.cs
+++ test/Cactus.MiniExcelConverterTest/StepDefinitions/MiniExcelConverterStepDefinitions.cs
@@ -1,5 +1,6 @@
 using Cactus.ExcelConverter.MiniExcelConverter;
 using NUnit.Framework;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Cactus.MiniExcelConverterTest.StepDefinitions
 {
@@ -8,8 +9,10 @@ namespace Cactus.MiniExcelConverterTest.StepDefinitions
     {
         const string DEFAULT_FOLDER = "Features";
 
+        //string _GUID = string.Empty;
         string _excelFile = string.Empty;
         string _featureFile = string.Empty;
+
 
         [Given("I have an Excel file {string}")]
         public void GivenIHaveAnExcelFile(string excelFile)
@@ -20,7 +23,9 @@ namespace Cactus.MiniExcelConverterTest.StepDefinitions
         [Given("I have an Excel file {string} in the {string} folder")]
         public void GivenIHaveAnExcelFile(string excelFile, string folder)
         {
+            //_GUID = System.Guid.NewGuid().ToString();
             _excelFile = Path.Combine(folder, excelFile);
+
             if (!File.Exists(Path.Combine(folder, excelFile)))
             {
                 throw new FileNotFoundException(" File not found: " + excelFile);
@@ -37,9 +42,39 @@ namespace Cactus.MiniExcelConverterTest.StepDefinitions
         [Then("I should have a feature file {string}")]
         public void ThenIShouldHaveAFeatureFile(string featureFile)
         {
-            _featureFile = Path.Combine(DEFAULT_FOLDER, featureFile);
+            ThenIShouldHaveAFeatureFile(featureFile, DEFAULT_FOLDER);
+        }
+
+        [Then("I should have a feature file {string} in the {string} folder")]
+        public void ThenIShouldHaveAFeatureFile(string featureFile, string folder)
+        {
+            _featureFile = Path.Combine(folder, featureFile);
             Assert.IsTrue(File.Exists(_featureFile));
         }
+
+        [Then("the feature file should match with {string}")]
+        public void ThenTheFeatureFileShouldMatchWith(string expectedFile)
+        {
+            ThenTheFeatureFileShouldMatchWith(expectedFile, DEFAULT_FOLDER);
+        }
+
+        [Then("the feature file should match with {string} in the {string} folder")]
+        public void ThenTheFeatureFileShouldMatchWith(string expectedFile, string folder)
+        {
+            string _expectedFile = Path.Combine(folder, expectedFile);
+            //string actualFile = Path.Combine(folder, Path.GetFileName(_featureFile));
+            string expected = File.ReadAllText(_expectedFile);
+            string feature = File.ReadAllText(_featureFile);
+            Assert.AreEqual(expected, feature);
+        }
+
+        //private string RandomizeFileName(string fileName)
+        //{
+        //    return Path.Combine(Path.GetDirectoryName(fileName), Path.GetFileNameWithoutExtension(fileName) + _GUID + Path.GetExtension(fileName));
+        //}
+
+
+
 
     }
 }
