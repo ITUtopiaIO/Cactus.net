8c328f488c364aa480d07e507b98660b3a3d2b3e
Chunxing Shen <chunxing.shen@gmail.com>
2024-06-30 00:01:11 -0500

    #4 relocate DiffPlex to test folder

--- test/Cactus.CucumberTest/FileDiff.cs
+++ test/Cactus.CucumberTest/FileDiff.cs
@@ -5,20 +5,6 @@ namespace Cactus.CucumberTest
 {
     public class FileDiff
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
-        [Test]
-        public void Test1()
-        {
-            Differ differ = new Differ();
-            var diff = differ.CreateLineDiffs("Hello   world!", "Hello world!", true);
-            Assert.NotNull(diff);
-            Assert.That(diff.DiffBlocks.Count, Is.EqualTo(1));
-
-
-        }
     }
 }
---
+++ test/Cactus.CucumberTest/FileDiffTest.cs
@@ -0,0 +1,31 @@
+﻿using DiffPlex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cactus.CucumberTest
+{
+    internal class FileDiffTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Differ differ = new Differ();
+            var diff = differ.CreateLineDiffs("Hello   world!", "Hello world!", true);
+            Assert.NotNull(diff);
+            Assert.That(diff.DiffBlocks.Count, Is.EqualTo(1));
+
+
+        }
+    }
+
+
+}
