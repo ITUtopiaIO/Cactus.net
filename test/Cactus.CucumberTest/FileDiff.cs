9a9d68b11ba8e9e28d3c72cc7fa3f445df2549ff
Chunxing Shen <chunxing.shen@gmail.com>
2025-02-20 02:38:05 -0600

    cn-#5 format diff result display

--- test/Cactus.CucumberTest/FileDiff.cs
+++ test/Cactus.CucumberTest/FileDiff.cs
@@ -39,14 +39,50 @@ namespace Cactus.CucumberTest
             }
 
             Differ differ = new Differ();
-            var diff = differ.CreateLineDiffs(outputData.ToString(), expectedData.ToString(), true);
+            var diff = differ.CreateLineDiffs(expectedData.ToString(), outputData.ToString(), true);
             if (diff.DiffBlocks.Count == 0)
             {
                 return string.Empty;
             }
             else
             {
-                return "It's Different";
+                int padLengh = 15;
+                int showDiffCount = 5;
+                StringBuilder diffData = new StringBuilder();
+                diffData.AppendLine(diff.DiffBlocks.Count+" difference(s) found:");
+                for (int i = 1; i <= diff.DiffBlocks.Count && i<= showDiffCount; i++)
+                {
+                    var diffBlock = diff.DiffBlocks[i-1];
+                    diffData.AppendLine("Difference " + (i) + ":");
+                    diffData.AppendLine("Expected: ".PadLeft(padLengh) + diff.PiecesOld[diffBlock.DeleteStartA].Trim());
+                    for (int j = 2; j <= diffBlock.DeleteCountA && j<=showDiffCount; j++)
+                    {
+                        diffData.AppendLine("".PadLeft(padLengh) + diff.PiecesOld[diffBlock.DeleteStartA+j-1].Trim());
+
+                        if (j == showDiffCount && j < diffBlock.DeleteCountA)
+                        {
+                            diffData.AppendLine("".PadLeft(padLengh) + "...");
+                        }
+                    }
+
+                    diffData.AppendLine("Actual: ".PadLeft(padLengh) + diff.PiecesNew[diffBlock.InsertStartB].Trim());
+                    for (int j = 2; j <= diffBlock.InsertCountB && j <= showDiffCount; j++)
+                    {
+                        diffData.AppendLine("".PadLeft(padLengh) + diff.PiecesNew[diffBlock.InsertStartB+j-1].Trim());
+
+                        if (j == showDiffCount && j < diffBlock.InsertCountB)
+                        {
+                            diffData.AppendLine("".PadLeft(padLengh)+"...");
+                        }
+                    }
+
+                    if (i==showDiffCount && i < diff.DiffBlocks.Count)
+                    {
+                        diffData.AppendLine("More Differences ...");
+                    }
+
+                }
+                return diffData.ToString();
             }
         }
 
