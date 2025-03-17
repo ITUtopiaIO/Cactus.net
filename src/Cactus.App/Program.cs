5d4db25204b2d1405b5a9675d7e8d9aa4d077741
Chunxing Shen <chunxing.shen@gmail.com>
2025-03-17 00:10:57 -0500

    cn-#16 add converter command line

--- src/Cactus.App/Program.cs
+++ src/Cactus.App/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int numberOfArguments = args.Length;
+if (numberOfArguments == 0)
+{
+    Console.WriteLine("Please provide the path to the Excel file you want to convert.");
+    return;
+}
+
+string _excelFile = args[0];
+
+try
+{
+    Cactus.ExcelConverter.Converter converter = new Cactus.ExcelConverter.Converter();
+    String _featureFile = converter.ConvertExcelToFeature(_excelFile);
+    Console.WriteLine("Feature file created: " + _featureFile);
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred while converting the Excel file to a feature file.");
+    Console.WriteLine(ex.ToString());
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+
