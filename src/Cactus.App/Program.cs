eb3d7be9fead7a25721882f9a72fe28095a02644
Chunxing Shen <chunxing.shen@gmail.com>
2026-01-22 01:11:25 -0600

    cn-#16 Using Spectre.Console for command line app

--- src/Cactus.App/Program.cs
+++ src/Cactus.App/Program.cs
@@ -1,11 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Spectre.Console;
+using Spectre.Console.Cli;
 using System.Runtime.CompilerServices;
 
 int numberOfArguments = args.Length;
 if (numberOfArguments == 0)
 {
-    Console.WriteLine("Please provide the path to the Excel file you want to convert.");
+    AnsiConsole.WriteLine("Please provide the path to the Excel file you want to convert.");
     return;
 }
 
@@ -15,14 +17,13 @@ try
 {
     Cactus.ExcelConverter.Converter converter = new Cactus.ExcelConverter.Converter();
     String _featureFile = converter.ConvertExcelToFeature(_excelFile);
-    Console.WriteLine("Feature file created: " + _featureFile);
+    AnsiConsole.WriteLine("Feature file created: " + _featureFile);
 
 }
 catch (Exception ex)
 {
-    Console.WriteLine("An error occurred while converting the Excel file to a feature file.");
-    Console.WriteLine(ex.ToString());
-    Console.WriteLine(ex.Message);
+    AnsiConsole.WriteLine("An error occurred while converting the Excel file to a feature file.");
+    AnsiConsole.WriteException(ex);
     return;
 }
 
