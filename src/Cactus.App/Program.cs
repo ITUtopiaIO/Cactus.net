39f69bfee7e7ce99ceb915fbbb426ca9a6164111
Chunxing Shen <chunxing.shen@gmail.com>
2026-01-27 23:04:21 -0600

    cn-#16 Using Spectre.Console cli for command line app

--- src/Cactus.App/Program.cs
+++ src/Cactus.App/Program.cs
@@ -2,31 +2,49 @@
 
 using Spectre.Console;
 using Spectre.Console.Cli;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
-int numberOfArguments = args.Length;
-if (numberOfArguments == 0)
+var app = new CommandApp<CactusCommand>();
+
+return await app.RunAsync(args);
+
+
+public class CactusCommand : AsyncCommand<CactusCommand.Settings>
 {
-    AnsiConsole.WriteLine("Please provide the path to the Excel file you want to convert.");
-    return;
+    public class Settings : CommandSettings
+    {
+        [CommandArgument(0, "[FileOrPath]")]
+        public string? FileOrPath { get; set; }
+    }
+
+
+    public override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
+    {
+        var fileOrPath = settings.FileOrPath;
+
+        if (fileOrPath is null)
+        {
+            AnsiConsole.WriteLine("Please provide the path to the Excel file you want to convert.");
+            AnsiConsole.WriteLine("For Help: Cactus -h");
+            return -1;
+        }
+
+        AnsiConsole.MarkupLine($"[green]Processing file or path:[/] {fileOrPath}");
+
+        try
+        {
+            Cactus.ExcelConverter.Converter converter = new Cactus.ExcelConverter.Converter();
+            String _featureFile = converter.ConvertExcelToFeature(fileOrPath);
+            AnsiConsole.WriteLine("Feature file created: " + _featureFile);
+            return 0;
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteLine("An error occurred while converting the Excel file to a feature file.");
+            AnsiConsole.WriteException(ex);
+            return -2;
+        }
+    }
 }
-
-string _excelFile = args[0];
-
-try
-{
-    Cactus.ExcelConverter.Converter converter = new Cactus.ExcelConverter.Converter();
-    String _featureFile = converter.ConvertExcelToFeature(_excelFile);
-    AnsiConsole.WriteLine("Feature file created: " + _featureFile);
-
-}
-catch (Exception ex)
-{
-    AnsiConsole.WriteLine("An error occurred while converting the Excel file to a feature file.");
-    AnsiConsole.WriteException(ex);
-    return;
-}
-
-
-
 
