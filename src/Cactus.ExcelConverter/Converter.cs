2bd4c54ed5273708ef545864214f680d65488204
Chunxing Shen <chunxing.shen@gmail.com>
2026-01-29 00:03:39 -0600

    cn-#16 Add file exist check prior MiniExcel throw an error

--- src/Cactus.ExcelConverter/Converter.cs
+++ src/Cactus.ExcelConverter/Converter.cs
@@ -16,6 +16,11 @@ namespace Cactus.ExcelConverter
 
         public string ConvertExcelToFeature(string excelFileName, string extension=".feature")
         {
+            if (!File.Exists(excelFileName))
+            {
+                throw new FileNotFoundException("The specified Excel file does not exist: " + excelFileName);
+            }
+
             return converter.ConvertExcelToFeature(excelFileName, extension);
         }
     }
