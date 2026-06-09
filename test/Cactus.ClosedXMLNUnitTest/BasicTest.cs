c6c395a69eaf3a4ec9b7666e8f2a9dddcd266e43
Chunxing Shen <chunxing.shen@gmail.com>
2026-06-09 16:00:16 -0500

    cn-#23 Adding a testing case to NOTE: Custom formatted DateTime is treated as number at this point

--- test/Cactus.ClosedXMLNUnitTest/BasicTest.cs
+++ test/Cactus.ClosedXMLNUnitTest/BasicTest.cs
@@ -1,4 +1,6 @@
 using ClosedXML.Excel;
+using Cactus.Cucumber;
+using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace Cactus.ClosedXMLNUnitTest
 {
@@ -48,6 +50,42 @@ namespace Cactus.ClosedXMLNUnitTest
                         Assert.That(rows[0].Cell(2).GetString().Trim(), Is.EqualTo("Sheet"));
                         Assert.That(rows[1].Cell(1).GetString().Trim(), Is.EqualTo("A"));
                         Assert.That(rows[1].Cell(2).GetString().Trim(), Is.EqualTo("S"));
+                    }));
+                }
+            }
+        }
+
+        [Test]
+        public void TestReadExcelDateTime()
+        {
+            using var workbook = new XLWorkbook(_file);
+
+            foreach (var worksheet in workbook.Worksheets)
+            {
+                var rows = worksheet.RangeUsed()!.RowsUsed().ToList();
+
+                if (worksheet.Name == "DateTime")
+                {
+                    Assert.Multiple((System.Action)(() =>
+                    {
+                        var cell = worksheet.Cell(1, 1);
+                        Assert.That(cell.DataType, Is.EqualTo(XLDataType.DateTime));
+                        string formattedDateTime = Utility.FormatDateTime(cell.GetDateTime());
+                        Assert.That(formattedDateTime, Is.EqualTo(Utility.FormatDateTime(new DateTime(2026, 7, 4, 0, 0, 0))));
+
+                        cell = worksheet.Cell(2, 1);
+                        Assert.That(cell.DataType, Is.EqualTo(XLDataType.DateTime));
+                        formattedDateTime = Utility.FormatDateTime(cell.GetDateTime());
+                        Assert.That(formattedDateTime, Is.EqualTo(Utility.FormatDateTime(new DateTime(2026, 7, 1, 0, 0, 0))));
+
+                        cell = worksheet.Cell(3, 1);
+                        Assert.That(cell.DataType, Is.EqualTo(XLDataType.DateTime));
+                        formattedDateTime = Utility.FormatDateTime(cell.GetDateTime());
+                        Assert.That(formattedDateTime, Is.EqualTo(Utility.FormatDateTime(new DateTime(2026, 7, 1, 0, 0, 0))));
+
+                        //NOTE: Custom formated DateTime is treated as number at this point, need reformate the spreadsheet as date first to process
+                        cell = worksheet.Cell(4, 1);
+                        Assert.That(cell.DataType, Is.EqualTo(XLDataType.Number));
                     }));
                 }
             }
